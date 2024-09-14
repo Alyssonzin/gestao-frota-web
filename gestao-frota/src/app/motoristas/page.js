71948bb244e0c8b2db298b5270c0e174261a0f12
Alysson <alyssonempire84@gmail.com>
2024-09-14 11:25:19 -0300

    começar tela de administração de motoristas

--- gestao-frota/src/app/motoristas/page.js
+++ gestao-frota/src/app/motoristas/page.js
@@ -1,9 +1,24 @@
 
 
 export default function Motoristas() {
-    return(
+    return (
         <>
-            
+            <aside className="bg-slate-400 fixed">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/motoristas">Motoristas</a>
+                        </li>
+                        <li>
+                            <a href="/veiculos">Veículos</a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+
+            <main className="h-screen">
+                <h2 className="text-center">Miau</h2>
+            </main>
         </>
     );
 }
