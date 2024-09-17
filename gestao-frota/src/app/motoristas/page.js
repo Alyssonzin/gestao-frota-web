2f836bdd8324ed071ad14f116f6595d53c83ad2f
Alysson <alyssonempire84@gmail.com>
2024-09-17 11:52:42 -0300

    exibição da tabela de motoristas

--- gestao-frota/src/app/motoristas/page.js
+++ gestao-frota/src/app/motoristas/page.js
@@ -5,51 +5,56 @@ import Input from "@/components/created/Input";
 
 export default function Motoristas() {
     return (
-        <main className="flex">
-            <aside className="flex flex-col items-center h-screen w-1/12 shadow-inner bg-gray-400 p-2 pt-10">
-                <nav>
+        <main className="flex bg-gray-200">
+            <aside className="flex flex-col items-center min-h-screen min-w-20 shadow-inner bg-gray-400 p-5 pt-10">
+                <nav className="fixed">
                     <ul className="flex flex-col items-center space-y-8">
                         <li className="text-white">
                             <Link href="/motoristas">
-                                <UsersRound size={35} />
+                                <UsersRound size={30} />
                             </Link>
                         </li>
 
                         <li className="text-white">
                             <Link href="/veiculos">
-                                <CarFront size={40} />
+                                <CarFront size={35} />
                             </Link>
                         </li>
                     </ul>
                 </nav>
             </aside>
 
-            <section className="h-screen bg-gray-200 w-full p-4 space-y-6">
-                <div className="w-1/3">
-                    <Input id="nome" type="text" placeholder="Pesquisar" />
+            <section className="w-1/2 p-4 ml-8 space-y-6">
+                <div className="w-3/4">
+                    <Input id="pesquisa" type="text" placeholder="Pesquisar" />
                 </div>
 
-                <table className="space-y-4 w-1/2 bg-white">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nome</th>
-                            <th>CPF</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-center">
-                        <tr>
-                            <td>10</td>
-                            <td>Alysson Pereira dos Santos</td>
-                            <td>000.000.000-00</td>
-                        </tr>
-                        <tr>
-                            <td>10</td>
-                            <td>Alysson Pereira dos Santos</td>
-                            <td>000.000.000-00</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="space-y-4 bg-white max-h-96 overflow-y-scroll shadow-md rounded-xl p-2 pb-3">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>CPF</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center space-y-16">
+                            {
+                                Array.from({ length: 100 }).map((_, index) => (
+                                    <tr key={index} className="border-t">
+                                        <td>{index + 1}</td>
+                                        <td>Guilherme Silveira Coelho</td>
+                                        <td>999.999.999-99</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+
+                    <div className="sticky bottom-2 text-end mr-4">
+                        <Link href="/motoristas/novo" className="bg-green-500 hover:bg-green-600 transition duration-200 rounded-md text-white p-2">Novo +</Link>
+                    </div>
+                </div>
 
             </section>
         </main>
