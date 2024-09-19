7d68dd6385f6d7d0c69cf9ff886d70ad6aa71d36
Alysson <alyssonempire84@gmail.com>
2024-09-19 11:17:16 -0300

    componente NavAdmin criado

--- gestao-frota/src/app/motoristas/page.js
+++ gestao-frota/src/app/motoristas/page.js
@@ -1,27 +1,12 @@
 import Link from "next/link";
-import Image from "next/image";
-import { UsersRound, CarFront } from "lucide-react";
 import Input from "@/components/created/Input";
+import NavAdmin from "@/components/created/NavAdmin";
 
 export default function Motoristas() {
     return (
         <main className="flex h-screen bg-gray-200">
-            <aside className="flex flex-col items-center min-h-screen min-w-20 shadow-inner bg-gray-400 pt-10">
-                <nav className="sticky w-full">
-                    <ul className="flex flex-col items-center">
-                        <li className="w-full flex justify-center text-white hover:bg-gray-500 py-5">
-                            <Link href="/motoristas" className="flex justify-center h-full w-full">
-                                <UsersRound size={30} />
-                            </Link>
-                        </li>
-
-                        <li className="w-full flex justify-center text-white hover:bg-gray-500 py-5">
-                            <Link href="/veiculos" className="flex justify-center h-full w-full">
-                                <CarFront size={35} />
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
+            <aside className="flex flex-col items-center h-full min-w-20 shadow-inner bg-gray-400 pt-10">
+                <NavAdmin />
             </aside>
 
             <section className="w-1/2 p-4 ml-8 space-y-6">
@@ -29,7 +14,7 @@ export default function Motoristas() {
                     <Input id="pesquisa" type="text" placeholder="Pesquisar" />
                 </div>
 
-                <div className="space-y-4 bg-white h-[85%] overflow-y-scroll shadow-md rounded-xl p-2 pb-3">
+                <div className="flex flex-col space-y-4 bg-white h-[85%] overflow-y-scroll shadow-md rounded-xl p-2 pb-3">
                     <table className="w-full">
                         <thead>
                             <tr>
@@ -51,7 +36,7 @@ export default function Motoristas() {
                         </tbody>
                     </table>
 
-                    <div className="sticky bottom-2 text-end mr-4">
+                    <div className="self-end sticky bottom-2 text-end mr-4">
                         <Link href="/motoristas/novo" className="bg-green-500 hover:bg-green-600 transition duration-200 shadow-md rounded-md text-white p-2">Novo +</Link>
                     </div>
                 </div>
---
+++ gestao-frota/src/components/created/NavAdmin.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { UsersRound, CarFront } from 'lucide-react';
+
+export default function NavAdmin() {
+    return (
+        <nav className="sticky top-10 w-full">
+            <ul className="flex flex-col items-center">
+                <li className="w-full flex justify-center text-white hover:bg-gray-500">
+                    <Link href="/motoristas" className="flex justify-center h-full w-full py-5">
+                        <UsersRound size={30} />
+                    </Link>
+                </li>
+
+                <li className="w-full flex justify-center text-white hover:bg-gray-500">
+                    <Link href="/veiculos" className="flex justify-center h-full w-full py-5">
+                        <CarFront size={35} />
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
