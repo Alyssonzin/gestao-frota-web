4a52e813e6f718281b98cb891244fd8bf6be0a67
Alyssonzin <alyssonempire84@gmail.com>
2024-09-30 22:19:12 -0300

    Construção dos objetos do formulário de cadastro

--- gestao-frota/src/app/motoristas/novo/page.js
+++ gestao-frota/src/app/motoristas/novo/page.js
@@ -1,42 +1,114 @@
+"use client"
+
 import NavAdmin from "@/components/created/NavAdmin";
 import Input from "@/components/created/Input";
-import Link from "next/link";
+import { useState } from "react";
+import Footer from "@/components/created/Footer";
 
 export default function CadastrarMotorista() {
-    const headers = ['#', 'Modelo', 'Placa'];
+    const [motorista, setMotorista] = useState({
+        cpf: '',
+        nome: '',
+        telefone: '',
+        endereco: {
+            logradouro: '',
+            bairro: ''
+        }
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        if (name.includes('.')) {
+            const [key, subkey] = name.split('.');
+
+            setMotorista({
+                ...motorista,
+                [key]: {
+                    ...motorista[key],
+                    [subkey]: value
+                }
+            })
+            
+        } else {
+            setMotorista({
+                ...motorista,
+                [name]: value
+            })
+        }
+    }
+
+    const handleCreateMotorista = async () => {
+        console.log(motorista);
+    }
+
+
     return (
-        <main className="flex h-screen bg-gray-200">
-            <aside className="flex flex-col items-center h-full min-w-20 shadow-inner bg-gray-400 pt-10">
+        <>
+            <main className="flex h-screen bg-gray-200">
                 <NavAdmin />
-            </aside>
 
-            <section className="flex flex-col items-center w-full p-4 ml-8 space-y-6">
+                <section className="flex flex-col items-center w-full p-4 ml-8 space-y-6">
 
-                <h1 className="text-3xl font-bold text-center">Cadastro de motoristas</h1>
+                    <h1 className="text-3xl font-bold text-center">Cadastro de motoristas</h1>
 
-                <form className="flex flex-col space-y-4 justify-center items-center w-1/2">
-                    <div className="w-1/2">
-                        <Input type="text" id="cpf" placeholder="CPF"></Input>
-                    </div>
-                    <div className="w-1/2">
-                        <Input type="text" id="nome" placeholder="Nome"></Input>
-                    </div>
-                    <div className="w-1/2">
-                        <Input type="text" id="telefone" placeholder="Telefone"></Input>
-                    </div>
-                    <div className="w-1/2">
-                        <Input type="text" id="rua" placeholder="Logradouro"></Input>
-                    </div>
-                    <div className="w-1/2">
-                        <Input type="text" id="bairro" placeholder="Bairro"></Input>
-                    </div>
+                    <form className="flex flex-col space-y-4 justify-center items-center w-1/2">
+                        <div className="w-1/2">
+                            <Input
+                                type="text"
+                                name="cpf"
+                                id="cpf"
+                                placeholder="CPF"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="w-1/2">
+                            <Input
+                                type="text"
+                                name="nome"
+                                id="nome"
+                                placeholder="Nome"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="w-1/2">
+                            <Input
+                                type="text"
+                                name="telefone"
+                                id="telefone"
+                                placeholder="Telefone"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="w-1/2">
+                            <Input
+                                type="text"
+                                name="endereco.logradouro"
+                                id="logradouro"
+                                placeholder="Rua"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="w-1/2">
+                            <Input
+                                type="text"
+                                name="endereco.bairro"
+                                id="bairro"
+                                placeholder="Bairro"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                    <Link href="#" className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-1/2">Salvar</Link>
-                </form>
-
-
-
-            </section>
-        </main>
+                        <button
+                            type="button"
+                            onClick={handleCreateMotorista}
+                            className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-1/2">
+                            Salvar
+                        </button>
+                    </form>
+                </section>
+            </main>
+            <Footer />
+        </>
     )
 }
--- gestao-frota/src/app/veiculos/novo/page.js
+++ gestao-frota/src/app/veiculos/novo/page.js
@@ -1,9 +1,30 @@
+"use client"
+
 import NavAdmin from "@/components/created/NavAdmin";
 import Input from "@/components/created/Input";
-import Link from "next/link";
 import Footer from "@/components/created/Footer";
+import { useState } from "react";
 
 export default function CadastrarVeiculo() {
+    const [veiculo, setVeiculo] = useState({
+        modelo: '',
+        placa: '',
+        renavam: '',
+        cor: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setVeiculo({
+            ...veiculo,
+            [name]: value
+        })
+    }
+
+    const handleCreateVeiculo = async () => {
+        console.log(veiculo);
+    }
 
     return (
         <>
@@ -13,21 +34,47 @@ export default function CadastrarVeiculo() {
                     <h1 className="text-3xl font-bold text-center">Cadastro de veículos</h1>
                     <form className="flex flex-col space-y-4 justify-center items-center w-1/2">
                         <div className="w-1/2">
-                            <Input type="text" id="modelo" placeholder="Modelo"></Input>
+                            <Input
+                                type="text"
+                                onChange={handleChange}
+                                name="modelo"
+                                id="modelo"
+                                placeholder="Modelo"
+                            />
                         </div>
                         <div className="w-1/2">
-                            <Input type="text" id="placa" placeholder="Placa"></Input>
+                            <Input
+                                type="text"
+                                onChange={handleChange}
+                                name="placa"
+                                id="placa"
+                                placeholder="Placa"
+                            />
                         </div>
                         <div className="w-1/2">
-                            <Input type="text" id="renavam" placeholder="Renavam"></Input>
+                            <Input
+                                type="text"
+                                onChange={handleChange}
+                                name="renavam"
+                                id="renavam"
+                                placeholder="renavam"
+                            />
                         </div>
                         <div className="w-1/2">
-                            <Input type="text" id="cor" placeholder="Cor"></Input>
+                            <Input
+                                type="text"
+                                onChange={handleChange}
+                                name="cor"
+                                id="cor"
+                                placeholder="Cor"
+                            />
                         </div>
-                        <div className="w-1/2">
-                            <Input type="text" id="bairro" placeholder="Bairro"></Input>
-                        </div>
-                        <Link href="#" className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-1/2">Salvar</Link>
+                        <button
+                            type="button"
+                            onClick={handleCreateVeiculo}
+                            className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-1/2">
+                            Salvar
+                        </button>
                     </form>
                 </section>
             </main>
