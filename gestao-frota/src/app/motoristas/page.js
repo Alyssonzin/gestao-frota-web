f769aa96390b380effd1f7a0f294ab5c5e122e7e
Alysson <alyssonempire84@gmail.com>
2024-10-01 15:59:05 -0300

    função de pesquisa criada nas telas de cadastro

--- gestao-frota/src/app/motoristas/page.js
+++ gestao-frota/src/app/motoristas/page.js
@@ -4,8 +4,10 @@ import Input from "@/components/created/Input";
 import NavAdmin from "@/components/created/NavAdmin";
 import Table from "@/components/created/Table";
 import Footer from "@/components/created/Footer";
+import { useState } from "react";
 
 export default function Motoristas() {
+    const [pesquisa, setPesquisa] = useState('');
     const motoristas = [
         { id: 1, nome: 'João Paulo Formentin Elias Pavannati', cpf: '123.456.789-00' },
         { id: 2, nome: 'Maria', cpf: '987.654.321-00' },
@@ -16,6 +18,19 @@ export default function Motoristas() {
     const colunas = ['id', 'nome', 'cpf'];
     const headers = ['#', 'Nome', 'CPF'];
 
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setPesquisa(value);
+    }
+
+    //Busca ao pressionar ENTER
+    const handleSearch = (event) => {
+        const { key } = event;
+        if (key === 'Enter'){
+            console.log(pesquisa);
+        }
+    }
+
     return (
         <>
             <main className="flex h-screen bg-gray-200">
@@ -24,7 +39,15 @@ export default function Motoristas() {
                 <section className="w-full p-4 ml-8 space-y-6">
                     <div className="flex justify-between gap-6">
                         <div className="w-[90%]">
-                            <Input id="pesquisa" type="text" placeholder="Pesquisar" />
+                            <Input
+                                onChange={handleChange}
+                                onKeyDown={handleSearch}
+                                name="pesquisa"
+                                id="pesquisa"
+                                type="text"
+                                placeholder="Pesquisar"
+                                autocomplete="off"
+                            />
                         </div>
                         <Link href="/motoristas/novo" className="bg-green-500 hover:bg-green-600 transition duration-200 shadow-md rounded-md select-none text-white p-2">Novo +</Link>
                     </div>
--- gestao-frota/src/app/veiculos/page.js
+++ gestao-frota/src/app/veiculos/page.js
@@ -6,6 +6,7 @@ import Table from "@/components/created/Table";
 import Footer from "@/components/created/Footer";
 
 export default function Veiculos() {
+    const [pesquisa, setPesquisa] = useState('');
     const carros = [
         { id: 1, modelo: 'Gol', placa: 'ABC-1234' },
         { id: 2, modelo: 'Uno', placa: 'DEF-5678' },
@@ -26,39 +27,22 @@ export default function Veiculos() {
         { id: 1, modelo: 'Gol', placa: 'ABC-1234' },
         { id: 2, modelo: 'Uno', placa: 'DEF-5678' },
         { id: 3, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 4, modelo: 'Civic', placa: 'JKL-3456' },
-        { id: 5, modelo: 'Corolla', placa: 'MNO-7890' },
-        { id: 6, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 7, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 8, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 1, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 2, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 3, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 4, modelo: 'Civic', placa: 'JKL-3456' },
-        { id: 5, modelo: 'Corolla', placa: 'MNO-7890' },
-        { id: 6, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 7, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 8, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 1, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 2, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 3, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 4, modelo: 'Civic', placa: 'JKL-3456' },
-        { id: 5, modelo: 'Corolla', placa: 'MNO-7890' },
-        { id: 6, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 7, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 8, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 1, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 2, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 3, modelo: 'Celta', placa: 'GHI-9012' },
-        { id: 4, modelo: 'Civic', placa: 'JKL-3456' },
-        { id: 5, modelo: 'Corolla', placa: 'MNO-7890' },
-        { id: 6, modelo: 'Gol', placa: 'ABC-1234' },
-        { id: 7, modelo: 'Uno', placa: 'DEF-5678' },
-        { id: 8, modelo: 'Celta', placa: 'GHI-9012' },
+        { id: 4, modelo: 'Civic', placa: 'JKL-3456' }
     ];
     const colunas = ['id', 'modelo', 'placa'];
     const headers = ['#', 'Modelo', 'Placa'];
 
+    const handleChange = (event) => {
+        const { value } = event.target;
+        setPesquisa(value);
+    }
+
+    const handleSearch = (event) => {
+        const { key } = event;
+        if (key === 'Enter') {
+            console.log(pesquisa);
+        }
+    }
 
     return (
         <>
@@ -68,7 +52,15 @@ export default function Veiculos() {
                 <section className="w-full p-4 ml-8 space-y-6">
                     <div className="flex justify-between gap-6">
                         <div className="w-[90%]">
-                            <Input id="pesquisa" type="text" placeholder="Pesquisar" />
+                            <Input
+                                onChange={handleChange}
+                                onKeyDown={handleSearch}
+                                id="pesquisa"
+                                name="pesquisa"
+                                type="text"
+                                placeholder="Pesquisar"
+                                autocomplete="off"
+                            />
                         </div>
                         <Link href="/veiculos/novo" className="bg-green-500 hover:bg-green-600 transition duration-200 shadow-md rounded-md select-none text-white p-2">Novo +</Link>
                     </div>
