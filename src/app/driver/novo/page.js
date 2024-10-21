4b3199dde730f00f330c738944a5b00641f7622c
Alysson <alyssonempire84@gmail.com>
2024-10-21 15:06:30 -0300

    Remodelando o cadastro de veículos

--- src/app/driver/novo/page.js
+++ src/app/driver/novo/page.js
@@ -35,95 +35,100 @@ export default function CadastrarMotorista() {
 
                     <h1 className="text-3xl font-bold text-center">Cadastro de motoristas</h1>
 
-                    <form className="flex flex-col space-y-4 justify-center items-center w-1/2">
-                        <div className="w-1/2">
-                            <span>CPF:</span>
-                            <Input
-                                type="text"
-                                name="cpf"
-                                id="cpf"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Nome:</span>
-                            <Input
-                                type="text"
-                                name="nome"
-                                id="nome"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>CNH:</span>
-                            <Input
-                                type="text"
-                                name="cnh"
-                                id="cnh"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Data de nascimento:</span>
-                            <Input
-                                type="text"
-                                name="data_nasc"
-                                id="data_nasc"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Telefone:</span>
-                            <Input
-                                type="text"
-                                name="telefone"
-                                id="telefone"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Logradouro:</span>
-                            <Input
-                                type="text"
-                                name="logradouro"
-                                id="logradouro"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Bairro:</span>
-                            <Input
-                                type="text"
-                                name="bairro"
-                                id="bairro"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-                        <div className="w-1/2">
-                            <span>Cidade:</span>
-                            <Input
-                                type="text"
-                                name="cidade"
-                                id="cidade"
-                                onChange={handleChange}
-                                autocomplete="off"
-                            />
-                        </div>
-
+                    <div className="space-y-4">
+                        <form className="flex space-x-32 justify-center w-full">
+                            <div className="space-y-4">
+                                <div className="w-full">
+                                    <span>CPF:</span>
+                                    <Input
+                                        type="text"
+                                        name="cpf"
+                                        id="cpf"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>Nome:</span>
+                                    <Input
+                                        type="text"
+                                        name="nome"
+                                        id="nome"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>CNH:</span>
+                                    <Input
+                                        type="text"
+                                        name="cnh"
+                                        id="cnh"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>Data de nascimento:</span>
+                                    <Input
+                                        type="text"
+                                        name="data_nasc"
+                                        id="data_nasc"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                            </div>
+                            <div className="space-y-4">
+                                <div className="w-full">
+                                    <span>Telefone:</span>
+                                    <Input
+                                        type="text"
+                                        name="telefone"
+                                        id="telefone"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>Logradouro:</span>
+                                    <Input
+                                        type="text"
+                                        name="logradouro"
+                                        id="logradouro"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>Bairro:</span>
+                                    <Input
+                                        type="text"
+                                        name="bairro"
+                                        id="bairro"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <span>Cidade:</span>
+                                    <Input
+                                        type="text"
+                                        name="cidade"
+                                        id="cidade"
+                                        onChange={handleChange}
+                                        autocomplete="off"
+                                    />
+                                </div>
+                            </div>
+                        </form>
                         <button
                             type="button"
                             onClick={handleCreateMotorista}
-                            className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-1/2">
+                            className="bg-green-500 hover:bg-green-700 transition duration-200 font-bold text-center shadow-md text-white py-2 px-4 rounded w-full">
                             Salvar
                         </button>
-                    </form>
+                    </div>
                 </section>
             </main>
             <Footer />
