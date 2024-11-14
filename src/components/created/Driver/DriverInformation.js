e5fce8cc7de62e9233cf45c89d4e267a2489dba8
Alyssonzin <alyssonempire84@gmail.com>
2024-11-13 22:00:01 -0300

    Refactor layout and styling of DriverInformation and VehicleInformation components

--- src/components/created/Driver/DriverInformation.js
+++ src/components/created/Driver/DriverInformation.js
@@ -34,18 +34,19 @@ export default function DriverInformation({ driver }) {
                 <h2 className="text-2xl text-center font-semibold mt-4">{`${driver.user.name} ${driver.user.last_name}`}</h2>
             </div>
 
-            <div className="w-full">
+            <div className="w-full flex justify-center">
                 <div className="space-y-4">
-                    <h2 className="text-xl font-semibold text-gray-700 mb-4">Informações Pessoais</h2>
-                    <div className="bg-white p-4 rounded-lg shadow-sm border border-gray-200">
-                        <p><span className="font-bold text-gray-600">CPF:</span> {driver.user.cpf}</p>
-                        <p><span className="font-bold text-gray-600">Data de Nascimento:</span> {driver.user.birth_date}</p>
-                        <p><span className="font-bold text-gray-600">CNH:</span> {driver.cnh}</p>
-                        <p><span className="font-bold text-gray-600">Telefone:</span> {driver.user.phone}</p>
-                        <p><span className="font-bold text-gray-600">E-mail:</span> {driver.user.email}</p>
+                    <h2 className="text-xl font-semibold text-gray-700 mb-4 text-center">Informações Pessoais</h2>
+                    <div className="bg-white rounded-lg shadow-sm border border-gray-200 pr-80 pl-80 p-5">
+                        <p className="text-left"><span className="font-bold text-gray-600">CPF:</span> {driver.user.cpf}</p>
+                        <p className="text-left"><span className="font-bold text-gray-600">Data de Nascimento:</span> {driver.user.birth_date}</p>
+                        <p className="text-left"><span className="font-bold text-gray-600">CNH:</span> {driver.cnh}</p>
+                        <p className="text-left"><span className="font-bold text-gray-600">Telefone:</span> {driver.user.phone}</p>
+                        <p className="text-left"><span className="font-bold text-gray-600">E-mail:</span> {driver.user.email}</p>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- src/components/created/Vehicle/VehicleInformation.js
+++ src/components/created/Vehicle/VehicleInformation.js
@@ -5,7 +5,7 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 export default function VehicleInformation({ vehicle }) {
 
     return (
-<div className="space-y-10 w-full p-6 bg-gray-50 rounded-lg shadow-lg">
+        <div className="space-y-10 w-full p-6 bg-gray-50 rounded-lg shadow-lg">
             <div className="flex items-center flex-col mb-6">
                 <Carousel className="border-2 w-1/4 border-gray-300 rounded-lg">
                     <CarouselContent>
@@ -33,10 +33,10 @@ export default function VehicleInformation({ vehicle }) {
                 </Carousel>
             </div>
 
-            <div className="w-full">
+            <div className="w-full flex justify-center">
                 <div className="space-y-4">
-                    <h2 className="text-xl font-semibold text-gray-700 mb-4">Informações do Veículo</h2>
-                    <div className="bg-white p-4 rounded-lg shadow-sm border border-gray-200">
+                    <h2 className="text-xl font-semibold text-gray-700 mb-4 text-center">Informações do Veículo</h2>
+                    <div className="bg-white rounded-lg shadow-sm border border-gray-200 pr-80 pl-80 p-5">
                         <p><span className="font-bold text-gray-600">Marca:</span> {vehicle.brand}</p>
                         <p><span className="font-bold text-gray-600">Modelo:</span> {vehicle.model}</p>
                         <p><span className="font-bold text-gray-600">Ano:</span> {vehicle.year}</p>
