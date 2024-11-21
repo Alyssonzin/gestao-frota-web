dc663a61a583481f3b4dbc4b008ecdf9b4ad96d6
Alysson <alyssonempire84@gmail.com>
2024-11-21 11:29:54 -0300

    Adicionar funcionalidade de logout (inconsistente)

--- src/components/created/NavAdmin.js
+++ src/components/created/NavAdmin.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
-import { UsersRound, CarFront, UserRoundPen } from 'lucide-react';
-
+import { UsersRound, CarFront, UserRoundPen, LogOut } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 export default function NavAdmin() {
+    const router = useRouter();
+
+    const logOut = () => {
+        document.cookie = 'email=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+        router.push('/');
+    }
+
     return (
-        <aside className="flex flex-col items-center min-w-20 shadow-inner bg-gray-800 pt-10">
-            <nav className="sticky top-10 w-full">
-                <ul className="flex flex-col items-center">
+        <aside className="flex flex-col items-center min-w-20 shadow-inner bg-gray-800 pt-10 pb-5">
+            <nav className="flex flex-col justify-between sticky top-10 w-full h-full">
+                <ul className="flex flex-col">
                     <li className="w-full flex justify-center text-white hover:bg-gray-500">
                         <Link href="/driver" className="flex justify-center h-full w-full py-5">
                             <UsersRound size={30} />
@@ -24,6 +31,14 @@ export default function NavAdmin() {
                         </Link>
                     </li>
                 </ul>
+
+                <div>
+                    <button
+                        className="w-full flex justify-center text-white hover:bg-gray-500 py-5"
+                        onClick={logOut}>
+                        <LogOut size={30} />
+                    </button>
+                </div>
             </nav>
         </aside>
     );
--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-    //const sessionCookie = request.cookies.get('email');
+    const sessionCookie = request.cookies.get('email');
 
-    // if (!sessionCookie) {
-    //     const loginUrl = new URL('/login', request.url);
-    //     return NextResponse.redirect(loginUrl);
-    // }
+    if (!sessionCookie) {
+        const loginUrl = new URL('/login', request.url);
+        return NextResponse.redirect(loginUrl);
+    }
     return NextResponse.next();
 }
 
