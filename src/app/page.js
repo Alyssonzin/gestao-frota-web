c3f53c309d80c4773f234ac5c406049b95167f87
Alysson <alyssonempire84@gmail.com>
2025-01-07 09:57:49 -0300

    Atualizar nome da empresa e informações de contato no layout e rodapé

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center min-h-screen py-2">
 
         <h1 className="text-6xl font-bold select-none">
-          Bem-vindo ao <span className="text-blue-600">Drivers Company</span>
+          Bem-vindo ao <span className="text-blue-600">Meu Projeto</span>
         </h1>
         <p className="mt-3 text-2xl select-none">Sistema de Gestão de Frotas</p>
       </main>
--- src/components/created/Footer.js
+++ src/components/created/Footer.js
@@ -4,13 +4,13 @@ export default function Footer() {
     return (
         <footer className="flex justify-evenly items-center p-4 bg-gray-900 text-white text-sm">
             <div>
-                <Image src="/logomarca.jpg" alt="Drivers Company" width={100} height={100} />
+                <Image src="/logomarca.jpg" alt="Company" width={100} height={100} />
             </div>
 
             <div className="space-y-2">
-                <p>Rua José Francisco Alves, 558 - Araranguá - SC, CEP 88905-256</p>
-                <p>Contato: ( 51 ) 93300-0408</p>
-                <p>© 2035 Drivers Company</p>
+                <p>Rua das Flores, 999 - Sangão - SC, CEP 88717-000</p>
+                <p>Contato: ( 99 ) 9999-9999</p>
+                <p>© Footer</p>
             </div>
 
         </footer>
